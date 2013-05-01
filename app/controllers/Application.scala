a1f4c43a86759941b5d24ed8422ca547c93d4b3a
Anoop Hallur <anooprh@thoughtworks.com>
2013-05-01 16:21:22 +0530

    Completed Displaying existing ToDos

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,15 +1,25 @@
 package controllers
 
 import play.api._
+import data._
+import data.Forms._
 import play.api.mvc._
 
+import models.Task
+
 object Application extends Controller {
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
   
   def index = Action {
     Redirect(routes.Application.tasks())
   }
 
-  def tasks = TODO
+  def tasks = Action {
+    Ok(views.html.index(Task.all, taskForm))
+  }
 
   def newTask = TODO
 
